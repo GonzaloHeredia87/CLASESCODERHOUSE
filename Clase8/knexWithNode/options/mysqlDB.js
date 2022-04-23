e1d2f2b6ab51bc36cdd6f93781c1ca009991e73f
Gonzalo Heredia <gherediam87@gmail.com>
2022-04-23 14:43:24 -0400

    respaldo 23 de Abril

---
+++ Clase8/knexWithNode/options/mysqlDB.js
@@ -0,0 +1,12 @@
+const options = {
+    client: 'mysql',  
+    connection: {
+        host:'192.168.64.2',       
+        user:'root',       
+        password:'',
+        database:'mibase'
+    }
+}
+module.exports = {
+    options
+}
